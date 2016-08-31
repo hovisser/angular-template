7c7fb83f22a403661e8a5347ba6486b51e22577e
Hotze Visser <hotzevisser@sensemakers.nl>
2016-08-31 13:22:05 +0200

    Added index template and minified js files

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ var connect = require('gulp-connect')
 // requires browserify and vinyl-source-stream
 var browserify = require('browserify')
 var source = require('vinyl-source-stream')
+var buffer = require('vinyl-buffer');
+var rename = require("gulp-rename");
+var minify = require('gulp-minify');
+
 
 gulp.task('connect', function () {
 	connect.server({
@@ -18,9 +22,24 @@ gulp.task('browserify', function() {
     	// bundles it and creates a file called main.js
         .bundle()
         .pipe(source('main.js'))
+				.pipe(buffer())
+				.pipe(minify({
+					ext:{
+            src:'-debug.js',
+            min:'.js'
+        	},
+        	ignoreFiles: ['.combo.js', '-min.js']
+				}))
         // saves it the public/js/ directory
         .pipe(gulp.dest('./dist/js/'));
 })
+
+gulp.task('index', function(){
+    return gulp.src('./app/index.tpl.html')
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('./dist/'))
+})
+
 
 gulp.task('sass', function() {
 	return sass('./app/style/style.sass')
@@ -31,6 +50,7 @@ gulp.task('sass', function() {
 gulp.task('watch', function() {
 	gulp.watch('app/**/*.js', ['browserify'])
 	gulp.watch('app/style/style.sass', ['sass'])
+	gulp.watch('app/index.tpl.html', ['index'] )
 })
 
-gulp.task('default', ['browserify', 'sass','connect', 'watch'])
+gulp.task('default', ['index', 'browserify', 'sass','connect', 'watch'])
